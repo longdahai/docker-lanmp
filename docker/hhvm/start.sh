46ab32e1d7d581cb851e8b317298dd80b70cfe13
haiwei long <haiwei.free@gmail.com>
2016-10-13 10:19:36 +0800

    change hhvm

--- docker/hhvm/start.sh
+++ docker/hhvm/start.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# cd /var/www/html/
-# hhvm -m server -p 80
-
-cd /var/www/html/newpfm.59yx.com
-hhvm -m server -p 8080 -d server.ini
+cd /var/www/html
+hhvm -m server -p 80 -d hhvm.ini
