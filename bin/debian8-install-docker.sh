5fdd623d11a988d8cec542204371f1b92542c8b1
dahaitech <haiwei.free@gmail.com>
2016-12-01 15:30:10 +0800

    fixed source echo

--- bin/debian8-install-docker.sh
+++ bin/debian8-install-docker.sh
@@ -1,7 +1,7 @@
 apt-get update
 apt-get install apt-transport-https ca-certificates
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-deb https://apt.dockerproject.org/repo debian-jessie main
+echo "deb https://apt.dockerproject.org/repo debian-jessie main" | sudo tee /etc/apt/sources.list.d/docker.list
 apt-get update
 apt-cache policy docker-engine
 sudo apt-get install docker-engine
