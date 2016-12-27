eaac61820e1863aecb136c4cea77bd22e542a5d6
dahaitech <haiwei.free@gmail.com>
2016-12-27 18:37:47 +0800

    add py settool

--- bin/install-docker-compose-source.sh
+++ bin/install-docker-compose-source.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get install curl -y
+apt-get install curl python-setuptools -y
 html=$(curl 'https://github.com/docker/compose/releases')
 url=$(echo $html|grep -Po 'href="[^"]+"'|grep gz|head -n 1|sed -e 's/href="//;s/"//')
 url="https://github.com$url"
