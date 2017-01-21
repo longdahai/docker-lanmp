b5efb3331fbe1f2ae8a47bb53b2cede8ebc30576
haiwei long <haiwei.free@gmail.com>
2017-01-21 22:14:40 +0800

    add uninstall docker-py

--- bin/install-docker-compose-source.sh
+++ bin/install-docker-compose-source.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+#!/
 apt-get install curl python-setuptools -y
-html=$(curl 'https://github.com/docker/compose/releases')
+html=$(curl -s 'https://github.com/docker/compose/releases')
 url=$(echo $html|grep -Po 'href="[^"]+"'|grep gz|head -n 1|sed -e 's/href="//;s/"//')
 url="https://github.com$url"
 curl -Lo docker-compose.tar.gz $url
@@ -8,5 +9,7 @@ rm docker-compose -rf
 mkdir docker-compose
 tar xf docker-compose.tar.gz -C docker-compose --strip-components=1
 cd docker-compose
+pip uninstall docker-py
 python setup.py install
+cd ..
 rm docker-compose.tar.gz docker-compose -rf
