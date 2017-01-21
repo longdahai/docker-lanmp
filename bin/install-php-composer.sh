5c44d440ef242e870b96b043c5f7601f0f6eede9
haiwei long <haiwei.free@gmail.com>
2017-01-21 21:55:53 +0800

    fixed delete sha check

--- bin/install-php-composer.sh
+++ bin/install-php-composer.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
+
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === 'c32408bcd017c577ce80605420e5987ce947a5609e8443dd72cd3867cc3a0cf442e5bf4edddbcbe72246a953a6c48e21') { echo 'Installer verified';  } else { echo 'Installer corrupt'; unlink('composer-setup.php');  } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
 
